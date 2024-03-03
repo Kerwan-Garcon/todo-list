3c20f642708477bdeed909f93aa3c70f4f30b4d9
Kerwan <77628932+Kerwan-Garcon@users.noreply.github.com>
2024-03-03 11:18:51 +0100

    Update page.tsx
    Fix responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Todos from "@/components/todos/Todos";
 
 export default function Home() {
   return (
-    <main className="mx-auto w-1/3 py-8">
+    <main className="mx-auto w-1/2 xl:w-1/3 py-8">
       <Todos />
     </main>
   );
